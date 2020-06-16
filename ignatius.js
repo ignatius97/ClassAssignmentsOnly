1ce9de059d4050b2ff24252c70d9f07e562cb29c
ignatius97 <47562170+ignatius97@users.noreply.github.com>
2020-06-16 15:02:13 +0300

    Create ignatius.js

---
+++ ignatius.js
@@ -0,0 +1,49 @@
+//Declaring a variable name
+var name;
+//Assigning a string value "ozzy queen" to the name variable 
+name = " ozzy queen"
+//Calling the name variable "ozzy queen" to display in the console
+console.log(name);
+//Calling an html tag to create a next line in the console
+console.log("<br/>");
+//Declaring the name variable again
+var name;
+//Reassinging the name variable to new string value "ozzy hum"
+name = "ozzy hum"
+//Calling the name variable "ozzy hum" to display in the console
+console.log(name)
+//Declaring the name variable again
+var name;
+//Reassinging the name variable to new string value "sumi mike ozyy"
+name = "sumi mike ozyy"
+//Calling the name variable "sumi mike ozyy" to display in the console
+console.log(name)
+//Declaring the name variable again
+var name;
+//Reassinging the name variable to new string value "ozzy bp"
+name = " ozzy bp"
+//Calling the name variable "ozzy bp" to display in the console
+console.log(name)
+//Reassinging the name variable to new string value "Bridget ozzy"
+name = " Bridget ozzy"
+//Calling the name variable "Bridget ozzy" to display in the console
+console.log(name)
+//Declaring the variable age
+var age;
+//Declaring the name variable again
+var name;
+//Assinging a number value 50 to the variable age
+age = 50;
+//Reassinging the name variable to new string value "mirembe agaba kwesi"
+name = "mirembe agaba kwesi"
+//Calling a write function to desplay a sentence in the web browser
+document.write("My name is " +name +" "+ "and I am "+age+" "+" years old");
+
+//Declaring an undefined variable x
+var x;
+//Calling x to be desplayed in the web browser
+document.write(x);
+//Assinging the variable x a datatype of undefined
+var x = undefined;
+//Invoking a function log() to return x 
+console.log(x);
